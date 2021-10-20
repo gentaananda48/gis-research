0f2b004e4aceea19e3b9a2ebd311031f1ff72858
endangsaepudin <endang_saepudin@creanova.id>
2021-10-21 06:25:22 +0700

    update API RK

--- app/Http/Controllers/API/RencanaKerjaController.php
+++ app/Http/Controllers/API/RencanaKerjaController.php
@@ -449,129 +449,131 @@ class RencanaKerjaController extends Controller {
 	public function summary(Request $request){
     	set_time_limit(0);
         $rk = RencanaKerja::find($request->id);
-        $geofenceHelper = new GeofenceHelper;
-        $list_polygon = $geofenceHelper->createListPolygon('L', $rk->lokasi_kode);
-        $list = Lacak::where('ident', $rk->unit_source_device_id)->where('timestamp', '>=', strtotime($rk->jam_mulai))->where('timestamp', '<=', strtotime($rk->jam_selesai))->orderBy('timestamp', 'ASC')->get();
-        $is_started = false;
-        $waktu_berhenti = 0;
-        $ritase = 1;
-        $list_movement = [];
-        foreach($list AS $k=>$v){
-            $lokasi         = $geofenceHelper->checkLocation($list_polygon, $v->position_latitude, $v->position_longitude);
-            $waktu_tempuh   = ($k==0) ? 0 : round(abs($v->timestamp - $list[$k-1]->timestamp),2);
-            $nozzle_kanan   = $v->ain_1 != null ? $v->ain_1 : 0;
-            $nozzle_kiri    = $v->ain_2 != null ? $v->ain_2 : 0;
-            $width          = ($nozzle_kanan > 12.63 ? 18 : 0) + ($nozzle_kiri > 12.63 ? 18 : 0);
-            $lebar_kanan    = ($nozzle_kanan > 12.63 ? 18 : 0);
-            $lebar_kiri     = ($nozzle_kiri > 12.63 ? 18 : 0);
-            $width          = ($nozzle_kanan > 12.63 ? 18 : 0) + ($nozzle_kiri > 12.63 ? 18 : 0);
-            $jarak_tempuh   = ($k==0) ? 0 : round(abs($v->vehicle_mileage - $list[$k-1]->vehicle_mileage),3);
-            $jarak_spray_kanan     = ($k==0) ? 0 : ($list[$k-1]->ain_1 > 12.63 ? $jarak_tempuh : 0);
-            $jarak_spray_kiri     = ($k==0) ? 0 : ($list[$k-1]->ain_2 > 12.63 ? $jarak_tempuh : 0);
-            if(!empty($lokasi) && $width >= 18) {
-                $is_started = true;
-                $obj = (object) [
-                    'timestamp'                 => $v->timestamp,
-                    'lokasi'                    => $lokasi,
-                    'position_latitude'         => $v->position_latitude,
-                    'position_longitude'        => $v->position_longitude,
-                    'vehicle_mileage'           => $v->vehicle_mileage,
-                    'nozzle_kanan'              => $nozzle_kanan,
-                    'nozzle_kiri'               => $nozzle_kiri,
-                    'width'                     => $width,
-                    'jarak_spray_kanan'         => $jarak_spray_kanan,
-                    'jarak_spray_kiri'          => $jarak_spray_kiri,
-                ];
-                if(array_key_exists($ritase, $list_movement)){
-                    $list_movement[$ritase]['list_gps'][] = $obj;
-                    $list_movement[$ritase]['jarak_spray_kanan'] += $jarak_spray_kanan;
-                    $list_movement[$ritase]['jarak_spray_kiri'] += $jarak_spray_kiri;
-                } else {
-                    $list_movement[$ritase] = [
-                        'list_gps'          => [$obj],
-                        'jarak_tempuh'      => 0,
-                        'jam_mulai'         => 0,
-                        'jam_selesai'       => 0,
-                        'waktu_tempuh'      => 0,
-                        'kecepatan'         => 0,
-                        'jarak_spray_kanan' => $jarak_spray_kanan,
-                        'jarak_spray_kiri'  => $jarak_spray_kiri
-                    ];
-                }
-                $waktu_berhenti = 0;
-            } else {
-                $waktu_berhenti += $waktu_tempuh;
-            }
-            if($is_started && $waktu_berhenti>=240){
-                $ritase += 1;
-                $is_started = false;
-            }
+        if(empty($rk->jam_laporan)) {
+	        $geofenceHelper = new GeofenceHelper;
+	        $list_polygon = $geofenceHelper->createListPolygon('L', $rk->lokasi_kode);
+	        $list = Lacak::where('ident', $rk->unit_source_device_id)->where('timestamp', '>=', strtotime($rk->jam_mulai))->where('timestamp', '<=', strtotime($rk->jam_selesai))->orderBy('timestamp', 'ASC')->get();
+	        $is_started = false;
+	        $waktu_berhenti = 0;
+	        $ritase = 1;
+	        $list_movement = [];
+	        foreach($list AS $k=>$v){
+	            $lokasi         = $geofenceHelper->checkLocation($list_polygon, $v->position_latitude, $v->position_longitude);
+	            $waktu_tempuh   = ($k==0) ? 0 : round(abs($v->timestamp - $list[$k-1]->timestamp),2);
+	            $nozzle_kanan   = $v->ain_1 != null ? $v->ain_1 : 0;
+	            $nozzle_kiri    = $v->ain_2 != null ? $v->ain_2 : 0;
+	            $width          = ($nozzle_kanan > 12.63 ? 18 : 0) + ($nozzle_kiri > 12.63 ? 18 : 0);
+	            $lebar_kanan    = ($nozzle_kanan > 12.63 ? 18 : 0);
+	            $lebar_kiri     = ($nozzle_kiri > 12.63 ? 18 : 0);
+	            $width          = ($nozzle_kanan > 12.63 ? 18 : 0) + ($nozzle_kiri > 12.63 ? 18 : 0);
+	            $jarak_tempuh   = ($k==0) ? 0 : round(abs($v->vehicle_mileage - $list[$k-1]->vehicle_mileage),3);
+	            $jarak_spray_kanan     = ($k==0) ? 0 : ($list[$k-1]->ain_1 > 12.63 ? $jarak_tempuh : 0);
+	            $jarak_spray_kiri     = ($k==0) ? 0 : ($list[$k-1]->ain_2 > 12.63 ? $jarak_tempuh : 0);
+	            if(!empty($lokasi) && $width >= 18) {
+	                $is_started = true;
+	                $obj = (object) [
+	                    'timestamp'                 => $v->timestamp,
+	                    'lokasi'                    => $lokasi,
+	                    'position_latitude'         => $v->position_latitude,
+	                    'position_longitude'        => $v->position_longitude,
+	                    'vehicle_mileage'           => $v->vehicle_mileage,
+	                    'nozzle_kanan'              => $nozzle_kanan,
+	                    'nozzle_kiri'               => $nozzle_kiri,
+	                    'width'                     => $width,
+	                    'jarak_spray_kanan'         => $jarak_spray_kanan,
+	                    'jarak_spray_kiri'          => $jarak_spray_kiri,
+	                ];
+	                if(array_key_exists($ritase, $list_movement)){
+	                    $list_movement[$ritase]['list_gps'][] = $obj;
+	                    $list_movement[$ritase]['jarak_spray_kanan'] += $jarak_spray_kanan;
+	                    $list_movement[$ritase]['jarak_spray_kiri'] += $jarak_spray_kiri;
+	                } else {
+	                    $list_movement[$ritase] = [
+	                        'list_gps'          => [$obj],
+	                        'jarak_tempuh'      => 0,
+	                        'jam_mulai'         => 0,
+	                        'jam_selesai'       => 0,
+	                        'waktu_tempuh'      => 0,
+	                        'kecepatan'         => 0,
+	                        'jarak_spray_kanan' => $jarak_spray_kanan,
+	                        'jarak_spray_kiri'  => $jarak_spray_kiri
+	                    ];
+	                }
+	                $waktu_berhenti = 0;
+	            } else {
+	                $waktu_berhenti += $waktu_tempuh;
+	            }
+	            if($is_started && $waktu_berhenti>=240){
+	                $ritase += 1;
+	                $is_started = false;
+	            }
+	        }
+	        $jarak_tempuh_total   = 0;
+	        $waktu_tempuh_total   = 0;
+	        $kecepatan_total      = 0;
+	        $jarak_spray_kanan_total   = 0;
+	        $jarak_spray_kiri_total   = 0;
+	        foreach($list_movement as $k=>$v){
+	            $list_gps = $v['list_gps'];
+	            if(count($list_gps)>0){
+	                $mileage1       = $list_gps[0]->vehicle_mileage;
+	                $mileage2       = count($list_gps) > 1 ? $list_gps[count($list_gps)-1]->vehicle_mileage : $mileage1;
+	                $timestamp1     = $list_gps[0]->timestamp;
+	                $timestamp2     = count($list_gps) > 1 ? $list_gps[count($list_gps)-1]->timestamp : $timestamp1;
+	                $jarak_tempuh   = round(abs($mileage2 - $mileage1),3);
+	                $waktu_tempuh   = round(abs($timestamp2 - $timestamp1),2);
+	                $kecepatan      = $waktu_tempuh > 0 ? round($jarak_tempuh / ($waktu_tempuh/3600),2) : 0;
+	                $list_movement[$k]['jarak_tempuh']  = $jarak_tempuh;
+	                $list_movement[$k]['jam_mulai']     = $timestamp1;
+	                $list_movement[$k]['jam_selesai']   = $timestamp2;
+	                $list_movement[$k]['waktu_tempuh']  = $waktu_tempuh;
+	                $list_movement[$k]['kecepatan']     = $kecepatan;
+	                $jarak_tempuh_total += $jarak_tempuh;
+	                $waktu_tempuh_total += $waktu_tempuh;
+	            }
+	            $stop_time = $k > 1 ? $list_movement[$k]['jam_mulai'] - $list_movement[$k-1]['jam_selesai'] : 0;
+	            $jarak_spray_kanan_total += $v['jarak_spray_kanan']; 
+	            $jarak_spray_kiri_total += $v['jarak_spray_kiri']; 
+	            $aktivitas = Aktivitas::find($rk->aktivitas_id);
+	            $list_rs = ReportStatus::get();
+	            $this->saveRKS($rk->id, $k, $aktivitas->grup_id, $rk->aktivitas_id, $rk->nozzle_id, $rk->volume_id, 1, $kecepatan);
+
+	            $luas_spray_total = ($v['jarak_spray_kanan'] * 1000 * 18 + $v['jarak_spray_kiri'] * 1000 * 18)/10000;
+	            $luas_standard_spray = 8000 / $rk->volume - 0.012 * (8000 / $rk->volume);
+	            $overlapping = ($luas_spray_total / $luas_standard_spray - 1)* 100;
+	            $this->saveRKS($rk->id, $k, $aktivitas->grup_id, $rk->aktivitas_id, $rk->nozzle_id, $rk->volume_id, 2, $overlapping);
+
+	            $this->saveRKS($rk->id, $k, $aktivitas->grup_id, $rk->aktivitas_id, $rk->nozzle_id, $rk->volume_id, 3, round($waktu_tempuh/60,2));
+
+	            $ketepatan_dosis = 100 - $overlapping;
+	            $this->saveRKS($rk->id, $k, $aktivitas->grup_id, $rk->aktivitas_id, $rk->nozzle_id, $rk->volume_id, 4, $ketepatan_dosis);
+
+	            $golden_time = date('H:i:s', $list_movement[$k]['jam_mulai']);
+	            $this->saveRKS($rk->id, $k, $aktivitas->grup_id, $rk->aktivitas_id, $rk->nozzle_id, $rk->volume_id, 5, $golden_time);
+
+	            $wing_level = 1.3;
+	            $this->saveRKS($rk->id, $k, $aktivitas->grup_id, $rk->aktivitas_id, $rk->nozzle_id, $rk->volume_id, 6, $wing_level);
+
+	            $this->saveRKS($rk->id, $k, $aktivitas->grup_id, $rk->aktivitas_id, $rk->nozzle_id, $rk->volume_id, 999, 0);
+	        } 
+	        $jam_mulai          = count($list_movement) > 0 ? $list_movement[1]['jam_mulai'] : 0;
+	        $jam_selesai        = count($list_movement) > 1 ? $list_movement[count($list_movement)]['jam_selesai'] : $jam_mulai;
+	        $kecepatan_total    = round($jarak_tempuh_total / ($waktu_tempuh_total/3600),2); 
+	        $luas_spray_total = ($jarak_spray_kanan_total * 1000 * 18 + $jarak_spray_kiri_total * 1000 * 18)/10000;
+
+	        $area_not_spray = 0;
+	        $this->saveRKS($rk->id, 999, $aktivitas->grup_id, $rk->aktivitas_id, $rk->nozzle_id, $rk->volume_id, 7, $area_not_spray);
+	        $this->saveRKS($rk->id, 999999, $aktivitas->grup_id, $rk->aktivitas_id, $rk->nozzle_id, $rk->volume_id, 999999, 0);
+
+	        $list_rks = RencanaKerjaSummary::where('rk_id', $rk->id)->orderBy('ritase', 'ASC')->orderBy('id', 'ASC')->get();
+
+	        // // Jarak tempuh: Dihitung mulai spray sd stop spray ( m)
+	        // //Luas aplikasi spray total: (Jarak tempuh x 1000) x (36/10.000)
+	        // // Area overlapping: 1 - ( luas peta lok/ luas aplikasi spray total)
+	        // // Ketepatan dosis spray(%) 100%  - prosen overlapping
+	        // // Satu ritase: Waktu, jarak dan lebar semprot per satu tangki boom sprayer ( 8000 liter)
+	        // // Waktu tunggu antar rit: Waktu yg dihasilkan saat tidak ada aktivitas spray dr rit sblmnya ke start spray rit berikutnya
         }
-        $jarak_tempuh_total   = 0;
-        $waktu_tempuh_total   = 0;
-        $kecepatan_total      = 0;
-        $jarak_spray_kanan_total   = 0;
-        $jarak_spray_kiri_total   = 0;
-        foreach($list_movement as $k=>$v){
-            $list_gps = $v['list_gps'];
-            if(count($list_gps)>0){
-                $mileage1       = $list_gps[0]->vehicle_mileage;
-                $mileage2       = count($list_gps) > 1 ? $list_gps[count($list_gps)-1]->vehicle_mileage : $mileage1;
-                $timestamp1     = $list_gps[0]->timestamp;
-                $timestamp2     = count($list_gps) > 1 ? $list_gps[count($list_gps)-1]->timestamp : $timestamp1;
-                $jarak_tempuh   = round(abs($mileage2 - $mileage1),3);
-                $waktu_tempuh   = round(abs($timestamp2 - $timestamp1),2);
-                $kecepatan      = $waktu_tempuh > 0 ? round($jarak_tempuh / ($waktu_tempuh/3600),2) : 0;
-                $list_movement[$k]['jarak_tempuh']  = $jarak_tempuh;
-                $list_movement[$k]['jam_mulai']     = $timestamp1;
-                $list_movement[$k]['jam_selesai']   = $timestamp2;
-                $list_movement[$k]['waktu_tempuh']  = $waktu_tempuh;
-                $list_movement[$k]['kecepatan']     = $kecepatan;
-                $jarak_tempuh_total += $jarak_tempuh;
-                $waktu_tempuh_total += $waktu_tempuh;
-            }
-            $stop_time = $k > 1 ? $list_movement[$k]['jam_mulai'] - $list_movement[$k-1]['jam_selesai'] : 0;
-            $jarak_spray_kanan_total += $v['jarak_spray_kanan']; 
-            $jarak_spray_kiri_total += $v['jarak_spray_kiri']; 
-            $aktivitas = Aktivitas::find($rk->aktivitas_id);
-            $list_rs = ReportStatus::get();
-            $this->saveRKS($rk->id, $k, $aktivitas->grup_id, $rk->aktivitas_id, $rk->nozzle_id, $rk->volume_id, 1, $kecepatan);
-
-            $luas_spray_total = ($v['jarak_spray_kanan'] * 1000 * 18 + $v['jarak_spray_kiri'] * 1000 * 18)/10000;
-            $luas_standard_spray = 8000 / $rk->volume - 0.012 * (8000 / $rk->volume);
-            $overlapping = ($luas_spray_total / $luas_standard_spray - 1)* 100;
-            $this->saveRKS($rk->id, $k, $aktivitas->grup_id, $rk->aktivitas_id, $rk->nozzle_id, $rk->volume_id, 2, $overlapping);
-
-            $this->saveRKS($rk->id, $k, $aktivitas->grup_id, $rk->aktivitas_id, $rk->nozzle_id, $rk->volume_id, 3, round($waktu_tempuh/60,2));
-
-            $ketepatan_dosis = 100 - $overlapping;
-            $this->saveRKS($rk->id, $k, $aktivitas->grup_id, $rk->aktivitas_id, $rk->nozzle_id, $rk->volume_id, 4, $ketepatan_dosis);
-
-            $golden_time = date('H:i:s', $list_movement[$k]['jam_mulai']);
-            $this->saveRKS($rk->id, $k, $aktivitas->grup_id, $rk->aktivitas_id, $rk->nozzle_id, $rk->volume_id, 5, $golden_time);
-
-            $wing_level = 1.3;
-            $this->saveRKS($rk->id, $k, $aktivitas->grup_id, $rk->aktivitas_id, $rk->nozzle_id, $rk->volume_id, 6, $wing_level);
-
-            $this->saveRKS($rk->id, $k, $aktivitas->grup_id, $rk->aktivitas_id, $rk->nozzle_id, $rk->volume_id, 999, 0);
-        } 
-        $jam_mulai          = count($list_movement) > 0 ? $list_movement[1]['jam_mulai'] : 0;
-        $jam_selesai        = count($list_movement) > 1 ? $list_movement[count($list_movement)]['jam_selesai'] : $jam_mulai;
-        $kecepatan_total    = round($jarak_tempuh_total / ($waktu_tempuh_total/3600),2); 
-        $luas_spray_total = ($jarak_spray_kanan_total * 1000 * 18 + $jarak_spray_kiri_total * 1000 * 18)/10000;
-
-        $area_not_spray = 0;
-        $this->saveRKS($rk->id, 999, $aktivitas->grup_id, $rk->aktivitas_id, $rk->nozzle_id, $rk->volume_id, 7, $area_not_spray);
-        $this->saveRKS($rk->id, 999999, $aktivitas->grup_id, $rk->aktivitas_id, $rk->nozzle_id, $rk->volume_id, 999999, 0);
-
-        $list_rks = RencanaKerjaSummary::where('rk_id', $rk->id)->orderBy('ritase', 'ASC')->orderBy('id', 'ASC')->get();
-
-        // // Jarak tempuh: Dihitung mulai spray sd stop spray ( m)
-        // //Luas aplikasi spray total: (Jarak tempuh x 1000) x (36/10.000)
-        // // Area overlapping: 1 - ( luas peta lok/ luas aplikasi spray total)
-        // // Ketepatan dosis spray(%) 100%  - prosen overlapping
-        // // Satu ritase: Waktu, jarak dan lebar semprot per satu tangki boom sprayer ( 8000 liter)
-        // // Waktu tunggu antar rit: Waktu yg dihasilkan saat tidak ada aktivitas spray dr rit sblmnya ke start spray rit berikutnya
 
         $rks = RencanaKerjaSummary::where('rk_id', $rk->id)->orderBy('ritase', 'ASC')->orderBy('id', 'ASC')->get();
     	$list_rks = [];
@@ -594,8 +596,7 @@ class RencanaKerjaController extends Controller {
       		'message' 	=> '', 
       		'data' 		=> [
 				'rk' 		=> $rk,
-				'rks' 		=> $list_rks2,
-				'summary'	=> $list_rks
+				'rks' 		=> $list_rks2
 			]
     	]);
 	} 
@@ -714,7 +715,7 @@ class RencanaKerjaController extends Controller {
 	      	$rk = RencanaKerja::find($request->id);
 	      	$status_id_lama 	= $rk->status_id;
 	      	$status_nama_lama 	= $rk->status_nama;
-	      	$rk->status_id 		= 2;
+	      	$rk->status_id 		= 4;
 	      	$status 	 		= Status::find($rk->status_id);
 	      	$rk->status_nama 	= $status->nama;
 	      	$rk->status_urutan  = $status->urutan;
