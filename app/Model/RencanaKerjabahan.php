48f49008810ae9c36ab677ec3b43db79299b4bab
endangsaepudin <endang_saepudin@creanova.id>
2021-11-17 06:52:07 +0700

    update RKB

---
+++ app/Model/RencanaKerjabahan.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model;
+use App\Model\BaseModel;
+
+class RencanaKerjaBahan extends BaseModel
+{
+    protected $table = 'rencana_kerja_bahan';
+	
+}
