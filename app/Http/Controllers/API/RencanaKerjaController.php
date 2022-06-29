20209e4d34f4ea2b0870e08b915acc9ad776ecc5
endangsaepudin <endang_saepudin@creanova.id>
2022-06-30 06:56:29 +0700

    update API Rencana Kerja

--- app/Http/Controllers/API/RencanaKerjaController.php
+++ app/Http/Controllers/API/RencanaKerjaController.php
@@ -91,6 +91,7 @@ class RencanaKerjaController extends Controller {
     }
 
     public function sync_down(Request $request){
+    	$user = $this->guard()->user();
         $updated_at = !empty($request->updated_at) ? $request->updated_at : '1900-01-01 00:00:00';
         $list_rk = RencanaKerja::whereRaw("(operator_id = ".$user->id." OR mixing_operator_id = ".$user->id." OR driver_id = ".$user->id." OR kasie_id = ".$user->id.")")
         	->where('updated_at', '>', $updated_at)->get();
