a99ad1a91bf0b5197962c0c017741ab6a41b2311
endangsaepudin <endang_saepudin@creanova.id>
2023-01-25 08:47:36 +0700

    update Report Summary

--- resources/views/report/rencana_kerja/playback.blade.php
+++ resources/views/report/rencana_kerja/playback.blade.php
@@ -104,14 +104,22 @@
 			            <tr>
 				            <td>{{$v['ritase']}}</td>
 				            @foreach($summary->header as $k2=>$v2)
+					        @if($k2==4 || $k2==5)
+					        <th>{{ doubleval($v['parameter_'.$k2]) <= 2 ? 'N/A': $v['parameter_'.$k2] }}</th>
+				            @else
 					        <th>{{$v['parameter_'.$k2]}}</th>
+					        @endif
 					        @endforeach
 			            </tr>
 			        	@endforeach
 				        <tr>
 					        <th>Rata-rata</th>
 					        @foreach($summary->rata2 as $v)
+					        @if($k==4 || $k==5)
+					        <th>{{ doubleval($v) <= 2 ? 'N/A': $v }}</th>
+				            @else
 					        <th>{{$v}}</th>
+					        @endif
 					        @endforeach
 				        </tr>
 				        <tr>
