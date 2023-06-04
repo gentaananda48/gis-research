6d74c466e11283c1a33c9403995f2e75e64e006c
erdhyka <erdhyka@Erdhyka-MacBook-Pro.local>
2023-06-04 21:32:40 +0700

    add device timestamp

--- app/Console/Commands/SaveJsonFile.php
+++ app/Console/Commands/SaveJsonFile.php
@@ -71,6 +71,7 @@ class SaveJsonFile extends Command
                             }
     
                             $temp['utc_timestamp'] = $data->utc_timestamp ? $data->utc_timestamp:null;
+                            $temp['device_timestamp'] = $data->utc_timestamp_tablet ? $data->utc_timestamp_tablet:null;
                             $temp['microcontroller_id'] = $data->microcontroller_id ? $data->microcontroller_id:null;
                             $temp['latitude'] = $data->latitude ? $data->latitude:null;
                             $temp['longitude'] = $data->longitude ? $data->longitude:null;
