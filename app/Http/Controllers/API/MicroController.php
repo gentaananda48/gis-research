e1022a19d15ab2e94285a2c23b3e2777ff5f5638
erdhyka <erdhyka@ip-192-168-246-169.ap-southeast-1.compute.internal>
2023-07-26 01:24:47 +0700

    run artisan via code

--- app/Http/Controllers/API/MicroController.php
+++ app/Http/Controllers/API/MicroController.php
@@ -9,6 +9,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Hash;
 use File;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 
@@ -228,106 +229,7 @@ class MicroController extends Controller {
     }
 
     public function cronJson(Request $request){
-        try {
-            $path = public_path('upload/database');
-            $files = File::files($path);
-            if (count($files) > 0) {
-                // \Log::info($files[0]);
-                foreach($files as $tempFile){
-                    $pathArchive = public_path('upload/archive');
-
-                    if (!file_exists($pathArchive)) {
-                        mkdir($pathArchive, 0777, true);
-                    }
-
-                    $year = date('Y');
-                    $month = date('m');
-                    $date = date('d');
-                    $pathArchiveMonth = public_path('upload/archive/'.$year.$month.$date);
-
-                    if (!file_exists($pathArchiveMonth)) {
-                        mkdir($pathArchiveMonth, 0777, true);
-                    }
-
-                    // get name from file
-                    $name = basename($tempFile);
-                    $getData = json_decode(file_get_contents($tempFile));
-
-                    try {
-                        if ($getData) {
-                            foreach ($getData as $data) {
-                                if (!isset($data->utc_timestamp_tablet)) {
-                                    continue;
-                                }
-
-                                $cekTable = \DB::table("lacak_".$data->source_device_id)->where('device_timestamp',$data->utc_timestamp_tablet)->first();
-                                if($cekTable){
-                                        continue;
-                                }
-        
-                                $temp['utc_timestamp'] = $data->utc_timestamp ? $data->utc_timestamp:null;
-
-                                if (isset($data->utc_timestamp_tablet)) {
-                                    $temp['device_timestamp'] = $data->utc_timestamp_tablet ? $data->utc_timestamp_tablet:null;
-                                }
-
-                                $temp['microcontroller_id'] = $data->microcontroller_id ? $data->microcontroller_id:null;
-                                $temp['latitude'] = $data->latitude ? $data->latitude:null;
-                                $temp['longitude'] = $data->longitude ? $data->longitude:null;
-                                $temp['speed'] = $data->speed ? $data->speed:null;
-                                $temp['altitude'] = $data->altitude ? $data->altitude:null;
-                                $temp['arm_height_left'] = $data->arm_height_left ? $data->arm_height_left:null;
-                                $temp['arm_height_right'] = $data->arm_height_right ? $data->arm_height_right:null;
-                                $temp['temperature_left'] = $data->temperature_left ? $data->temperature_left:null;
-                                $temp['temperature_right'] = $data->temperature_right ? $data->temperature_right:null;
-                                $temp['pump_switch_left'] = $data->pump_switch_left ? $data->pump_switch_left:0;
-                                $temp['pump_switch_right'] = $data->pump_switch_right ? $data->pump_switch_right:0;
-                                $temp['pump_switch_main'] = $data->pump_switch_main ? $data->pump_switch_main:0;
-                                $temp['flow_meter_left'] = $data->flow_meter_left ? $data->flow_meter_left:null;
-                                $temp['flow_meter_right'] = $data->flow_meter_right ? $data->flow_meter_right:null;
-                                $temp['tank_level'] = $data->tank_level ? $data->tank_level:null;
-                                $temp['oil'] = $data->oil ? $data->oil:null;
-                                $temp['gas'] = $data->gas ? $data->gas:null;
-                                $temp['homogenity'] = $data->homogenity ? $data->homogenity:null;
-                                $temp['bearing'] = isset($data->bearing) ? $data->bearing:null;
-                                $temp['box_id'] = $data->box_id ? $data->box_id:null;
-                                $temp['unit_label'] = $data->unit_label ? $data->unit_label:null;
-                                $temp['created_at'] = date('Y-m-d H:i:s');
-                                $temp['processed'] = 0;
-                                $report_date = date('His', $data->utc_timestamp) <= '050000' ? date('Y-m-d', strtotime("-1 day", $data->utc_timestamp)) : date('Y-m-d', $data->utc_timestamp);
-                                $temp['report_date'] = $report_date;
-        
-                                \DB::table("lacak_".$data->source_device_id)->insert($temp);
-                            }
-        
-                            // pindahkan dari publik database ke archive
-                            File::move($path.'/'.$name, $pathArchiveMonth.'/'.$name);
-
-                            \Log::info("save success");
-                            $this->info('save succes');
-                        }else{
-                            unlink($tempFile);
-                        }
-                    } catch (\Throwable $th) {
-                        \Log::info($th->getMessage());
-                        \Log::info($th->getLine());
-                    }
-                }
-            }
-
-            return response()->json([
-                "status" => true,
-                "message" => "File successfully uploaded",
-                "data" => array()
-            ]);
-        } catch (\Exception $e) {
-            Log::error($e->getMessage());
-            return response()->json([
-                "status" => false,
-                "message" => $e->getMessage(),
-                "data" => array()
-            ]);
-        }
+        Artisan::call('save:jsonfile');
         
     }
 
