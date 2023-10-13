cc310e878709189f50838904194d5d0cda3a56db
innovation dept <innovation.dept@gg-foods.com>
2023-10-13 10:24:06 +0700

    fix controller for images

--- app/Http/Controllers/API/ImageController.php
+++ app/Http/Controllers/API/ImageController.php
@@ -9,7 +9,7 @@ class ImageController extends Controller
 {
     public function __construct()
     {
-        // $this->middleware('auth:api', []);
+        $this->middleware('auth:api', []);
     }
 
     public function getImages()
